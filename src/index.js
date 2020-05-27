59c0164aab7c4c66da725768db636ed192c85b3f
庄骥南 <a492073467@gmail.com>
2020-05-28 02:38:54 +0800

    另一个兼容性更高的版本

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ function _bind(asThis, ...args1) {
     function resultFn(...args2) {
         return fn.call(this instanceof resultFn ? this : asThis, ...args1, ...args2)
     }
+
     resultFn.prototype = fn.prototype
 
     return resultFn
@@ -11,13 +12,16 @@ function _bind(asThis, ...args1) {
 
 
 slice = Array.prototype.slice
-function bind(asThis){
+
+function bind(asThis) {
     var fn = this
-    var args1 = slice.call(arguments,1)
+    var args1 = slice.call(arguments, 1)
+
     function resultFn() {
-        var args2 = slice.call(arguments,0)
+        var args2 = slice.call(arguments, 0)
         return fn.apply(this instanceof resultFn ? this : asThis, args1.concat(args2))
     }
+
     resultFn.prototype = fn.prototype
 
     return resultFn
